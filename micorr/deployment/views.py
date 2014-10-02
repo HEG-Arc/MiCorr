ae539c6a91ff0ab524f621fc193358e7211c209c
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2014-10-02 16:06:19 +0200

    Travis webhook test

--- micorr/deployment/views.py
+++ micorr/deployment/views.py
@@ -48,11 +48,11 @@ def pull(request):
         logger.debug("Authorization: %s" % request.META.get('HTTP_AUTHORIZATION'))
         logger.debug("Travis-Repo-Slug: %s" % request.META.get('HTTP_TRAVIS_REPO_SLUG'))
         logger.debug("Result: %s" % payload)
-        # logger.debug("ID: %s" % payload['id'])
-        # logger.debug("Type: %s" % payload['type'])
-        # logger.debug("Commit: %s" % payload['commit'])
-        # logger.debug("Branch: %s" % payload['branch'])
-        # logger.debug("Message: %s" % payload['message'])
+        logger.debug("ID: %s" % payload['id'])
+        logger.debug("Type: %s" % payload['type'])
+        logger.debug("Commit: %s" % payload['commit'])
+        logger.debug("Branch: %s" % payload['branch'])
+        logger.debug("Message: %s" % payload['message'])
     else:
         logger.debug("Called outside a POST request")
         raise Http404
