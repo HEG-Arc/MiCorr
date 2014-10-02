96bfbde2334c8932a3c4e9c13ae1376efd5cf740
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2014-10-02 15:11:36 +0200

    Travis webhook test

--- micorr/deployment/views.py
+++ micorr/deployment/views.py
@@ -44,14 +44,15 @@ logger = logging.getLogger(__name__)
 def pull(request):
     if request.POST:
         #travis = json.loads(request.body)
-        payload = request.POST['payload']
+        result = request.POST
         logger.debug("Authorization: %s" % request.META.get('HTTP_AUTHORIZATION'))
         logger.debug("Travis-Repo-Slug: %s" % request.META.get('HTTP_TRAVIS_REPO_SLUG'))
-        logger.debug("ID: %s" % payload.get('id'))
-        logger.debug("Type: %s" % payload.get('type'))
-        logger.debug("Commit: %s" % payload.get('commit'))
-        logger.debug("Branch: %s" % payload.get('branch'))
-        logger.debug("Message: %s" % payload.get('message'))
+        logger.debug("Result: %s" % result)
+        # logger.debug("ID: %s" % payload.get('id'))
+        # logger.debug("Type: %s" % payload.get('type'))
+        # logger.debug("Commit: %s" % payload.get('commit'))
+        # logger.debug("Branch: %s" % payload.get('branch'))
+        # logger.debug("Message: %s" % payload.get('message'))
     else:
         logger.debug("Called outside a POST request")
         raise Http404
