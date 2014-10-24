6d8980fdf608b4e667912dbc6f2fdc14cdeab51a
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2014-10-24 09:04:23 +0200

    Initial creation of the contacts application

---
+++ micorr/contacts/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+# __init__.py
+#
+# Copyright (C) 2014 HES-SO//HEG Arc
+#
+# Author(s): Cédric Gaspoz <cedric.gaspoz@he-arc.ch>
+#
+# This file is part of MiCorr.
+#
+# MiCorr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MiCorr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MiCorr. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+The contact application is designed to display, edit, create and delete information regarding
+people or organisation that have a link to MiCorr (owner or artefacts, redactors, users, ...).
+A contact can be a user (able to log in MiCorr) but it is not mandatory.
+"""
