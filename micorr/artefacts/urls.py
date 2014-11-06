c27d7fe8d81f1ad3d4abfad99774f5ece6e36ba2
antoinerosselet <antoine.rosselet@he-arc.ch>
2014-11-06 17:09:15 +0100

    login_required added in the artefacts urls (Requirement #30)

--- micorr/artefacts/urls.py
+++ micorr/artefacts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from .views import ArtefactsListView, ArtefactsDetailView, ArtefactsUpdateView, ArtefactsDeleteView, ArtefactsCreateView
 
 urlpatterns = patterns('',
     url(r'^$', ArtefactsListView.as_view(), name='artefact-list'),
     url(r'^(?P<pk>\d+)/$', ArtefactsDetailView.as_view(), name='artefact-detail'),
-    url(r'^(?P<pk>\d+)/update/$', ArtefactsUpdateView.as_view(), name='artefact-update'),
-    url(r'^(?P<pk>\d+)/delete/$', ArtefactsDeleteView.as_view(), name='artefact-delete'),
-    url(r'^create/$', ArtefactsCreateView.as_view(), name='artefact-create'),
+    url(r'^(?P<pk>\d+)/update/$', login_required(ArtefactsUpdateView.as_view()), name='artefact-update'),
+    url(r'^(?P<pk>\d+)/delete/$', login_required(ArtefactsDeleteView.as_view()), name='artefact-delete'),
+    url(r'^create/$', login_required(ArtefactsCreateView.as_view()), name='artefact-create'),
 )
