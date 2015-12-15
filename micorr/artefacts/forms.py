2bdb9e88837ce6444a13aca0090e7d5dda4ef17c
antoinerosselet <antoine.rosselet@he-arc.ch>
2015-12-15 16:01:45 +0100

    changed Corrosion forms filter

--- micorr/artefacts/forms.py
+++ micorr/artefacts/forms.py
@@ -95,7 +95,7 @@ class ArtefactFilter(django_filters.FilterSet):
     A filter which appears on top of the artefacts list
     """
     metal1 = django_filters.ModelChoiceFilter(label='Metal Family', queryset=Metal.objects.filter(id__in=Artefact.objects.values_list("metal1").distinct()), empty_label='All Metal Families')
-    corrosion_form = django_filters.ModelChoiceFilter(label='Corrosion Forms', queryset=CorrosionForm.objects.filter(form__contains="Uniform"), empty_label='All Corrosion Forms')
+    corrosion_form = django_filters.ModelChoiceFilter(label='Corrosion Forms', queryset=CorrosionCategory.objects.filter(name__in=Artefact.objects.values_list("corrosion_form__form")), empty_label='All Corrosion Forms')
     environment = django_filters.ModelChoiceFilter(label='Environment', queryset=Environment.objects.all(), empty_label='All Environments')
 
     class Meta:
