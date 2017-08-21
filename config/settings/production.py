34ebe31563f165052ab3c630c6b69c56c1900d65
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2017-08-21 17:18:22 +0200

    Adds back version display in footer base in production based on APP_GIT_REV set in .env file #MC-275 add work Development 1h
    django dealer package that we use to display version information in templates (for ex. in about page footer)
    by default extracted git revision from source repository. But git repository is not part of the production django container so we:
    
     - switch DEALER_TYPE from auto to env in production settings.
     - update start-docker-prod.sh to parse APP_GIT_REV (used by sentry) and generate DEALER_TAG and DEALER_REVISION env vars
       expected by dealer

--- config/settings/production.py
+++ config/settings/production.py
@@ -182,3 +182,4 @@ ADMIN_URL = env('DJANGO_ADMIN_URL')
 # Your production stuff: Below this line define 3rd party library settings
 INSTALLED_APPS += ("deployment", )
 
+DEALER_TYPE = 'env' # expect DEALER_TAG and DEALER_REVISION environment variables to be set in .env
--- start-docker-prod.sh
+++ start-docker-prod.sh
@@ -2,7 +2,30 @@
 APP_GIT_REV=$(git describe --tags)
 echo "update .env file with MiCorr git revision:"
 echo "APP_GIT_REV=$APP_GIT_REV"
-sed -i~ .env -e "s/^APP_GIT_REV=.*$/APP_GIT_REV=$APP_GIT_REV/"
-grep -q "APP_GIT_REV=value" .env || echo "APP_GIT_REV=$APP_GIT_REV" >> .env
+
+# Parse APP_GIT_REV TAG-NBCOMMIT-gCOMMIT to extract dealer package env vars
+EXP_STATEMENT=$(echo $APP_GIT_REV | sed -e 's/\(^.*\)-.*.g\(.*$\)/DEALER_TAG=\1 DEALER_REVISION=\2/')
+
+# echo $EXP_STATEMENT
+if [ "$EXP_STATEMENT" == "$APP_GIT_REV" ]
+ then
+    # git describe result is a tag only (HEAD at tag) so sed did not find pattern
+    # and did not modify echo $APP_GIT_REV output
+    export DEALER_TAG=$APP_GIT_REV DEALER_REVISION=$APP_GIT_REV
+ else
+    # sed extracted DEALER_TAG and DEALER_REVISION from APP_GIT_REV
+    export $EXP_STATEMENT
+ fi
+echo DEALER_TAG=$DEALER_TAG
+echo DEALER_REVISION=$DEALER_REVISION
+
+# update or create variables in.env file
+update_var() {
+    sed -i~ .env -e "s/^$1=.*$/$1=$2/"
+    grep -q "$1" .env || echo "$1=$2" >> .env
+}
+update_var APP_GIT_REV $APP_GIT_REV
+update_var DEALER_TAG $DEALER_TAG
+update_var DEALER_REVISION $DEALER_REVISION
 echo "build and start docker-compose production containers"
 docker-compose up -d --build
