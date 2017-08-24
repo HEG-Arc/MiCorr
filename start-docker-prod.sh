7e7b47e8bd18517b6542fc522dde2deaa25706a7
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2017-08-24 15:27:44 +0200

    #MC-275 Add Work Development 20m Updates version information production env. vars
    - Updates DEALER_TAG and DEALER_REVISION to retrieved commit number even when HEAD is on TAG
      so that we get full version information and correct link to commit on github in footer.
      we'll now get for example :
         DEALER_TAG=v1.0-5-ge1e7100 DEALER_REVISION=e1e7100
      instead of:
         DEALER_TAG=v1.0 DEALER_REVISION=e1e7100
      or worst:
         DEALER_TAG=v1.0 DEALER_REVISION=v1.0
    
    - also re-activate docker-compose up  build that was commented out in previous commit

--- start-docker-prod.sh
+++ start-docker-prod.sh
@@ -3,25 +3,17 @@ if [! -f compose/nginx/dhparams.pem]
 then
     openssl dhparam -out compose/nginx/dhparams.pem 2048
 fi
-APP_GIT_REV=$(git describe --tags)
+
+export APP_GIT_REV=$(git describe --tags)
+export APP_GIT_COMMIT=$(git rev-parse --short HEAD)
+
+export DEALER_TAG=$APP_GIT_REV # DEALER_TAG will show full describe version information in app (ie v1.0-5-ge1e7100)
+export DEALER_REVISION=$APP_GIT_COMMIT # DEALER_REVISION will allow to link to commit on github
+
 echo "update .env file with MiCorr git revision:"
 echo "APP_GIT_REV=$APP_GIT_REV"
-
-# Parse APP_GIT_REV TAG-NBCOMMIT-gCOMMIT to extract dealer package env vars
-EXP_STATEMENT=$(echo $APP_GIT_REV | sed -e 's/\(^.*\)-.*.g\(.*$\)/DEALER_TAG=\1 DEALER_REVISION=\2/')
-
-# echo $EXP_STATEMENT
-if [ "$EXP_STATEMENT" == "$APP_GIT_REV" ]
- then
-    # git describe result is a tag only (HEAD at tag) so sed did not find pattern
-    # and did not modify echo $APP_GIT_REV output
-    export DEALER_TAG=$APP_GIT_REV DEALER_REVISION=$APP_GIT_REV
- else
-    # sed extracted DEALER_TAG and DEALER_REVISION from APP_GIT_REV
-    export $EXP_STATEMENT
- fi
-echo DEALER_TAG=$DEALER_TAG
-echo DEALER_REVISION=$DEALER_REVISION
+echo "DEALER_TAG=$DEALER_TAG"
+echo "DEALER_REVISION=$DEALER_REVISION"
 
 # update or create variables in.env file
 update_var() {
@@ -32,4 +24,4 @@ update_var APP_GIT_REV $APP_GIT_REV
 update_var DEALER_TAG $DEALER_TAG
 update_var DEALER_REVISION $DEALER_REVISION
 echo "build and start docker-compose production containers"
-#docker-compose up -d --build
+docker-compose up -d --build
