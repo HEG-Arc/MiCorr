fc6476aec2ab6e2c3bccd27c5ed7017e5f41d6f2
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2017-10-31 22:40:17 +0100

    #MC-310 Add Work Development 1h fixes whitenoise 3.3.1 upgrade Exception change in ErrorSquashingStorage
     - previous commit did not handle the actual case.
     - seems that in django 1.11 collectstatic process changed (as reverting back to whitenoise 3.2.2 did not solve the issue) and now raises exceptions on missing file in post_process not url anymore.
      So we override post_process in similar way ignoring any MissingFileError exception

--- micorr/whitenoise/storage.py
+++ micorr/whitenoise/storage.py
@@ -8,5 +8,13 @@ class ErrorSquashingStorage(CompressedManifestStaticFilesStorage):
     def url(self, name, **kwargs):
         try:
             return super(ErrorSquashingStorage, self).url(name, **kwargs)
-        except MissingFileError:
+        except (MissingFileError, ValueError):
             return name
+
+    def post_process(self, *args, **kwargs):
+        files = super(ErrorSquashingStorage, self).post_process(*args, **kwargs)
+        for name, hashed_name, processed in files:
+            if isinstance(processed, MissingFileError):
+                processed = True
+            yield name, hashed_name, processed
+
