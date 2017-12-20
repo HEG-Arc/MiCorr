b5f01dd9761a35b6544a56a965e385d0fad31d17
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2017-12-20 15:35:34 +0100

    Removes duplicated/conflicting help text - actually fixing Artefact update form author help text #MC-318 Add Work Development 2h ArtefactsUpdateForm refactoring and fix
    - ArtefactsUpdateForm refactoring:
     explicitely enumerate Model fields to include in form instead of excluding. to avoid risking to publish unwanted new fields
     (even though form template is hard coded for now)
    - removes redundant/conflicting help_text definition for Artefacts model fields
      this is allowed by using widgets dictionary Meta attribute to customize the widget of these fields
      without redefining them in the form (as it was done before)
    - removes unnecessary TinyMCE() duplicated attrs initialization arguments.
    
    - models.py and artefacts migration 0025:
      update Artefacts model help_texts for microstructure and authors (that are now the only reference in Artefact form)

--- micorr/artefacts/forms.py
+++ micorr/artefacts/forms.py
@@ -28,46 +28,55 @@ class MultipleSelectWithPop(forms.SelectMultiple):
 
 
 class ArtefactsUpdateForm(forms.ModelForm):
-    """
-    Update an existing artefact
-    """
-
-    complementary_information = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    macroscopic_text = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    sample_complementary_information = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    analyses_performed = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    metal_text = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    metal_complementary_information = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}),  required=False)
-    corrosion_text = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    corrosion_complementary_information = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    synthesis_text = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    conclusion_text = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    references_text = forms.CharField(widget=TinyMCE(attrs={'cols': 10, 'rows': 10}), required=False)
-    #object_name = forms.CharField(required=False, help_text='Name of the artefact')
-
-    #name = forms.ModelChoiceField(Object.objects, widget=SelectWithPop)
-    author = forms.ModelMultipleChoiceField(Contact.objects, widget=MultipleSelectWithPop, help_text='The author(s) of this file is (are) responsible for the information provided. Author(s) should provide their last name, initial of their first name and in brackets the abbreviation of their institutional affiliation, such as Degrigny C. (HE-Arc CR).', required=False)
-    type = forms.ModelChoiceField(Type.objects, widget=SelectWithPop, help_text='The name of the artefact, its typology', required=False)
-    origin = forms.ModelChoiceField(Origin.objects, widget=SelectWithPop, help_text='The place, city and country where the artefact comes from or the object to which the section considered belongs to', required=False)
-    recovering_date = forms.ModelChoiceField(RecoveringDate.objects, widget=SelectWithPop, help_text='The date of excavation for archaeological objects, of production and use for other artefacts', required=False)
-    chronology_period = forms.ModelChoiceField(ChronologyPeriod.objects, widget=SelectWithPop, help_text='The dating of the artefact', required=False)
-    environment = forms.ModelChoiceField(Environment.objects, widget=SelectWithPop, help_text='The environment where the artefact was found.', required=False)
-    location = forms.ModelChoiceField(Contact.objects, widget=SelectWithPop, help_text='The actual location of the artefact', required=False)
-    owner = forms.ModelChoiceField(Contact.objects, widget=SelectWithPop, help_text='The owner of the artefact', required=False)
-    alloy = forms.ModelChoiceField(Alloy.objects, widget=SelectWithPop, help_text='The alloy the artefact is made of', required=False)
-    technology = forms.ModelChoiceField(Technology.objects, widget=SelectWithPop, help_text='The manufacturing techniques used to produce the artefact', required=False)
-    sample_location = forms.ModelChoiceField(Contact.objects, widget=SelectWithPop, help_text='The actual location of the artefact sample', required=False)
-    responsible_institution = forms.ModelChoiceField(Contact.objects, widget=SelectWithPop, help_text='The responsible institution for the artefact sample', required=False)
-    microstructure = forms.ModelChoiceField(Microstructure.objects, widget=SelectWithPop, help_text='Microstructure of the metal', required=False)
-    metal1 = forms.ModelChoiceField(Metal.objects, widget=SelectWithPop, help_text='The primary metal element of the artefact', required=False)
-    metalx = forms.ModelMultipleChoiceField(Metal.objects, widget=MultipleSelectWithPop, help_text='The other metal elements of the artefact.', required=False)
-    corrosion_form = forms.ModelChoiceField(CorrosionForm.objects, widget=SelectWithPop, required=False)
-    corrosion_type = forms.ModelChoiceField(CorrosionType.objects, widget=SelectWithPop, help_text='', required=False)
-
 
     class Meta:
         model = Artefact
-        exclude = ['object',]
+        fk_fields = [ # hand picked from [f.name for f in Artefact._meta.get_fields() if f.db_type.im_class==ForeignKey]
+            'metal1',
+            'alloy',
+            'type',
+            'origin',
+            'recovering_date',
+            'chronology_period',
+            'environment',
+            'location',
+            'owner',
+            'technology',
+            'sample_location',
+            'responsible_institution',
+            'microstructure',
+            'corrosion_form',
+            'corrosion_type']
+        m2m_fields = ['author', 'metalx']
+
+        # Here we only customize widget of selected fk_fields and m2m_fields
+        # whereas redefining fields in the Form for this purpose would override
+        # all model attributes including help_text...
+        widgets = {f: SelectWithPop for f in fk_fields}
+        widgets.update({f: MultipleSelectWithPop for f in m2m_fields})
+
+        other_fields = [  # hand picked from [f.name for f in Artefact._meta.get_fields()]
+            'description',
+            'inventory_number',
+            'recorded_conservation_data',
+            'sample_description',
+            'sample_number',
+            'date_aim_sampling',
+            'object']
+        fields = fk_fields + m2m_fields + other_fields
+
+    # Non Artefact/form only fields (coming from related model Section )
+    complementary_information = forms.CharField(widget=TinyMCE(), required=False)
+    macroscopic_text = forms.CharField(widget=TinyMCE(), required=False)
+    sample_complementary_information = forms.CharField(widget=TinyMCE(), required=False)
+    analyses_performed = forms.CharField(widget=TinyMCE(), required=False)
+    metal_text = forms.CharField(widget=TinyMCE(), required=False)
+    metal_complementary_information = forms.CharField(widget=TinyMCE(), required=False)
+    corrosion_text = forms.CharField(widget=TinyMCE(), required=False)
+    corrosion_complementary_information = forms.CharField(widget=TinyMCE(),required=False)
+    synthesis_text = forms.CharField(widget=TinyMCE(), required=False)
+    conclusion_text = forms.CharField(widget=TinyMCE(), required=False)
+    references_text = forms.CharField(widget=TinyMCE(), required=False)
 
 class ArtefactsCreateForm(forms.ModelForm):
     """
--- micorr/artefacts/models.py
+++ micorr/artefacts/models.py
@@ -260,7 +260,7 @@ class Artefact(TimeStampedModel):
                                    help_text='The manufacturing techniques used to produce the artefact')
     sample_location = models.ForeignKey(Contact, blank=True, null=True, related_name='sample_location', help_text='The actual location of the artefact sample')
     responsible_institution = models.ForeignKey(Contact, blank=True, null=True, related_name='responsible_institution', help_text='The responsible institution for the artefact sample')
-    microstructure = models.ForeignKey(Microstructure, blank=True, null=True, help_text='A description of the metal: its composition, texture (porosity), hardness, microstructure revealed by etching and specific features (figures and tables are referred as Fig. 1, Table 1)')
+    microstructure = models.ForeignKey(Microstructure, blank=True, null=True, help_text='Microstructure of the metal')
     corrosion_form = models.ForeignKey(CorrosionForm, blank=True, null=True)
     corrosion_type = models.ForeignKey(CorrosionType, blank=True, null=True, help_text='')
     parent = models.ForeignKey('self', blank=True, null=True, help_text='The card from which this card is the child')
