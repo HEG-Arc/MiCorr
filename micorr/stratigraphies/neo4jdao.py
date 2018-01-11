HEG-Arc/MiCorr

commit f5bf6221aad6e9ead95d654a5cbd456e3d0b7636

#MC-217 Add Work Development 6h Implements secondary Component load/save/delete adding new "Component" label to Graph model
 showStrat.js:
  . getDetailedStratigraphy() add support for secondaryComponents load

  . refactoring: moves utility functions (getSubCharacteristicByFamily, getCharacteristicByFamilyMulti, getSubCharacteristicByFamilyMulti)
       that were not needed as controller interface on the $scope to local context
       removes boiler plage code repetition using Characteristic and SubCharacteristic constructors

strata.js: toJson() add support for  secondaryComponents

neo4jdao.py: adds load, save and delete support for components
  . getDatailedStratigrapy() : add secondaryComponent reading from graph
  . save() : add suport for secondaryComponents (in create_secondary_components() here trial/sample use of py2neo OGM  instead of cypher queries)
  . deleteAllStrataFromAStratigraphy() : adds Component removal plus optimization by using a single cypher request

micorr/stratigraphies/neo4jdao.py
@@ -124,6 +124,25 @@ class Neo4jDAO:
                 slist.append({'name': subCharact.uid, 'real_name': subCharact.real_name})
                 print("         " + subCharact.uid)
 
+            print ("======Components")
+            component_records = self.graph.cypher.execute(
+                "MATCH (sg:Stratigraphy {uid:{stratigraphy_uid}})-[POSSESSES]->(s:Strata { uid:{strata_uid} })-[r:INCLUDES]->(c:Component)-[IS_CONSTITUTED_BY]->(characteristic)\
+                 OPTIONAL MATCH (characteristic)-[BELONGS_TO]->(family:Family) RETURN s,c,characteristic,family ORDER BY c.name",
+                strata_uid=strata.uid,stratigraphy_uid=stratigraphy_uid)
+            secondary_components = [{'characteristics': [], 'subCharacteristics': []}]
+            for record in component_records:
+                if 'SubCharacteristic' in record.characteristic.labels:
+                    secondary_components[0]['subCharacteristics'].append(
+                        {'name': record.characteristic['uid'], 'real_name': record.characteristic['name']})
+                elif 'Characteristic' in record.characteristic.labels:
+                    secondary_components[0]['characteristics'].append(
+                        {'name': record.characteristic['uid'], 'family': record.family['uid'],
+                         'real_name': record.characteristic['name'],
+                         'order': record.characteristic['order'], 'visible': record.characteristic['visible']})
+                print("         " + str(record.characteristic))
+            if len(secondary_components[0]['characteristics']) or len(secondary_components[0]['subCharacteristics']):
+                st['secondaryComponents'] = secondary_components
+
             # Chaque strates a des interfaces
             print("======interface")
             interfaceName = self.graph.cypher.execute(
@@ -323,14 +342,16 @@ class Neo4jDAO:
     # @params nom de la stratigraphie
     # @returns
     def deleteAllStrataFromAStratigraphy(self, stratigraphy):
-        #supression des strates enfant
-        self.query = "MATCH (n:Stratigraphy)-[p:POSSESSES]->(b:Strata)-[i:IS_PARENT_OF]->(c:Strata) where n.uid='" + stratigraphy + "' optional match (c)-[z]-() DELETE z, i ,c"
-        self.graph.cypher.execute(self.query)
-        #supression des strates
-        self.query = "MATCH (n:Stratigraphy)-[p:POSSESSES]->(b:Strata)-[h:HAS_UPPER_INTERFACE]->(i:Interface) where n.uid='" + stratigraphy + "' optional match (i)-[x]-() optional match (b)-[y]-() optional match (b)-[z]-()  delete x, y, z, h, p, i, b"
-        self.graph.cypher.execute(self.query)
+        # supression des strates enfants, interfaces et components
+        self.graph.cypher.execute('''
+            MATCH (sg:Stratigraphy {uid:{stratigraphy}})-[:POSSESSES]->(parent_st:Strata)-[:IS_PARENT_OF]->(child_st:Strata)
+            DETACH DELETE child_st
+            WITH sg MATCH (sg)-[:POSSESSES]->(st:Strata)-[:HAS_UPPER_INTERFACE]->(i:Interface)
+            OPTIONAL MATCH (st)-[:INCLUDES]->(c:Component)
+            DETACH DELETE c,i,st
+        ''', stratigraphy=stratigraphy)
 
-    # supprime une stratigraphie
+    # # supprime une stratigraphie
     # @params nom de la stratigraphie
     # @returns
     def deleteStratigraphy(self, stratigraphy):
@@ -394,6 +415,32 @@ class Neo4jDAO:
         self.query = "MATCH (a:Strata),(b:Strata) WHERE a.uid = '" + strata + "' AND b.uid= '" + parentstrata + "' CREATE (b)-[:IS_PARENT_OF]->(a)"
         self.graph.cypher.execute(self.query)
 
+    def create_secondary_components(self, strata, components):
+        """
+        :param strata:
+        :param components:
+        :return:
+        """
+        if not len(components):
+            return
+        st = self.graph.find_one("Strata", "uid", strata)
+        for i, component in enumerate(components):
+            c = Node("Component", order=i)
+            stc = Relationship(st, "INCLUDES", c)
+            self.graph.create(c,st,stc)
+            if len(component):
+                for characteristic in component:
+                    # characteristic_node = self.graph.match(Node("Characteristic",uid=characteristic['name']))
+                    characteristic_node = self.graph.find_one("Characteristic","uid",characteristic['name'])
+                    # bad temp re find_one as we can't find without Label... use match instead or upgrade py2neo 3 with transaction support
+                    if not characteristic_node:
+                        characteristic_node = self.graph.find_one("SubCharacteristic", "uid", characteristic['name'])
+                    if characteristic_node:
+                        self.graph.create(Relationship(c, "IS_CONSTITUTED_BY", characteristic_node))
+                    else:
+                        print 'Characteristic:{} not found'.format(characteristic['name'])
+                # one liner fail not working for merge self.graph.create(*[Relationship(c, "IS_CONSTITUTED_BY", )) for characteristic in component])
+
     # retourne le nombre d'interface pour toutes les strates d'une stratigraphie
     # @params nom de la stratigraphie
     # @returns nombre d'interface pour toutes les strates d'une stratigrpahie
@@ -445,6 +492,8 @@ class Neo4jDAO:
                     for sc in s['characteristics']:
                         if len(sc) > 0:
                             self.attachCharacteristicToStrata(child_name, sc['name'])
+
+            self.create_secondary_components(strata_name,stratum['secondaryComponents'])
 
         return {'res': 1}
 

micorr/stratigraphies/static/micorr/scripts/ES2015/business/strata.js
@@ -502,35 +502,41 @@ class Strata {
     }
 
     toJson() {
-        var childStrata = [];
+        var childStrata = [],i;
 
-        var jsonStrata = {'name': this.getUid(), 'characteristics': [], 'interfaces': [], 'children': []};
+        var jsonStrata = {name: this.getUid(), characteristics: [], interfaces: [], children: [], secondaryComponents:[]};
 
         //On récupère les caractéristiques
-        for (var i = 0; i < this.characteristics.length; i++) {
+        for (i = 0; i < this.characteristics.length; i++) {
             if (!this.characteristics[i].isInterface()) {
-                jsonStrata.characteristics.push({'name': this.characteristics[i].getName()});
+                jsonStrata.characteristics.push({name: this.characteristics[i].getName()});
             }
         }
         //On récupère les sous caractéristiques
-        for (var i = 0; i < this.subCharacteristics.length; i++) {
-            jsonStrata.characteristics.push({'name': this.subCharacteristics[i].getUid()});
+        for (i = 0; i < this.subCharacteristics.length; i++) {
+            jsonStrata.characteristics.push({name: this.subCharacteristics[i].getUid()});
         }
 
         //On récupère les caractéristiques d'interface
-        for (var i = 0; i < this.characteristics.length; i++) {
+        for (i = 0; i < this.characteristics.length; i++) {
             if (this.characteristics[i].isInterface()) {
-                jsonStrata.interfaces.push({'name': this.characteristics[i].getName()});
+                jsonStrata.interfaces.push({name: this.characteristics[i].getName()});
             }
         }
 
         //On récupère les strates enfants si ce n'est pas une strate enfant
         if (!this.child) {
-            for (var i = 0; i < this.childStrata.length; i++) {
+            for (i = 0; i < this.childStrata.length; i++) {
                 jsonStrata.children.push(this.childStrata[i].toJson());
             }
         }
-
+        // secondaryComponents
+        for (let sc of this.secondaryComponents) {
+            let all_component_characteristics = sc.characteristics.map(c => ({name: c.getName()})
+            ).concat(sc.subCharacteristics.map(sc => ({name: sc.getUid()})));
+            if (all_component_characteristics.length)
+                jsonStrata.secondaryComponents.push(all_component_characteristics);
+        }
         return jsonStrata;
 
     }

micorr/stratigraphies/static/micorr/scripts/ES2015/business/subCharacteristic.js
@@ -6,13 +6,20 @@
  */
 
 class SubCharacteristic {
-
     constructor(family, sourceSC){
         if (family)
             this.family = family;
         if (sourceSC) {
-            this.uid = sourceSC.uid;
-            this.name = sourceSC.name;
+            if ('uid' in sourceSC)
+            {
+                this.uid = sourceSC.uid ;
+                this.name = sourceSC.name;
+            }
+            else
+            {
+                this.uid = sourceSC.name ;
+                this.name = sourceSC.real_name;
+            }
         }
     }
 

micorr/stratigraphies/static/micorr/scripts/business/strata.js
@@ -603,32 +603,63 @@
         }, {
             key: "toJson",
             value: function toJson() {
-                var childStrata = [];
+                var childStrata = [],
+                    i;
 
-                var jsonStrata = { 'name': this.getUid(), 'characteristics': [], 'interfaces': [], 'children': [] };
+                var jsonStrata = { name: this.getUid(), characteristics: [], interfaces: [], children: [], secondaryComponents: [] };
 
                 //On récupère les caractéristiques
-                for (var i = 0; i < this.characteristics.length; i++) {
+                for (i = 0; i < this.characteristics.length; i++) {
                     if (!this.characteristics[i].isInterface()) {
-                        jsonStrata.characteristics.push({ 'name': this.characteristics[i].getName() });
+                        jsonStrata.characteristics.push({ name: this.characteristics[i].getName() });
                     }
                 }
                 //On récupère les sous caractéristiques
-                for (var i = 0; i < this.subCharacteristics.length; i++) {
-                    jsonStrata.characteristics.push({ 'name': this.subCharacteristics[i].getUid() });
+                for (i = 0; i < this.subCharacteristics.length; i++) {
+                    jsonStrata.characteristics.push({ name: this.subCharacteristics[i].getUid() });
                 }
 
                 //On récupère les caractéristiques d'interface
-                for (var i = 0; i < this.characteristics.length; i++) {
+                for (i = 0; i < this.characteristics.length; i++) {
                     if (this.characteristics[i].isInterface()) {
-                        jsonStrata.interfaces.push({ 'name': this.characteristics[i].getName() });
+                        jsonStrata.interfaces.push({ name: this.characteristics[i].getName() });
                     }
                 }
 
                 //On récupère les strates enfants si ce n'est pas une strate enfant
                 if (!this.child) {
-                    for (var i = 0; i < this.childStrata.length; i++) {
+                    for (i = 0; i < this.childStrata.length; i++) {
                         jsonStrata.children.push(this.childStrata[i].toJson());
+                    }
+                }
+                // secondaryComponents
+                var _iteratorNormalCompletion4 = true;
+                var _didIteratorError4 = false;
+                var _iteratorError4 = undefined;
+
+                try {
+                    for (var _iterator4 = this.secondaryComponents[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+                        var sc = _step4.value;
+
+                        var all_component_characteristics = sc.characteristics.map(function (c) {
+                            return { name: c.getName() };
+                        }).concat(sc.subCharacteristics.map(function (sc) {
+                            return { name: sc.getUid() };
+                        }));
+                        if (all_component_characteristics.length) jsonStrata.secondaryComponents.push(all_component_characteristics);
+                    }
+                } catch (err) {
+                    _didIteratorError4 = true;
+                    _iteratorError4 = err;
+                } finally {
+                    try {
+                        if (!_iteratorNormalCompletion4 && _iterator4.return) {
+                            _iterator4.return();
+                        }
+                    } finally {
+                        if (_didIteratorError4) {
+                            throw _iteratorError4;
+                        }
                     }
                 }
 

micorr/stratigraphies/static/micorr/scripts/business/subCharacteristic.js
@@ -1,6 +1,6 @@
 (function (global, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["exports"], factory);
+        define(['exports'], factory);
     } else if (typeof exports !== "undefined") {
         factory(exports);
     } else {
@@ -11,7 +11,7 @@
         global.subCharacteristic = mod.exports;
     }
 })(this, function (exports) {
-    "use strict";
+    'use strict';
 
     Object.defineProperty(exports, "__esModule", {
         value: true
@@ -47,48 +47,53 @@
 
             if (family) this.family = family;
             if (sourceSC) {
-                this.uid = sourceSC.uid;
-                this.name = sourceSC.name;
+                if ('uid' in sourceSC) {
+                    this.uid = sourceSC.uid;
+                    this.name = sourceSC.name;
+                } else {
+                    this.uid = sourceSC.name;
+                    this.name = sourceSC.real_name;
+                }
             }
         }
 
         _createClass(SubCharacteristic, [{
-            key: "getId",
+            key: 'getId',
             value: function getId() {
                 return this.id;
             }
         }, {
-            key: "setId",
+            key: 'setId',
             value: function setId(id) {
                 this.id = id;
             }
         }, {
-            key: "getUid",
+            key: 'getUid',
             value: function getUid() {
                 return this.uid;
             }
         }, {
-            key: "setUid",
+            key: 'setUid',
             value: function setUid(uid) {
                 this.uid = uid;
             }
         }, {
-            key: "getName",
+            key: 'getName',
             value: function getName() {
                 return this.name;
             }
         }, {
-            key: "setName",
+            key: 'setName',
             value: function setName(name) {
                 this.name = name;
             }
         }, {
-            key: "getFamily",
+            key: 'getFamily',
             value: function getFamily() {
                 return this.family;
             }
         }, {
-            key: "setFamily",
+            key: 'setFamily',
             value: function setFamily(family) {
                 this.family = family;
             }

micorr/stratigraphies/static/micorr/scripts/business/subCharacteristic.js.map
@@ -1,1 +1,1 @@
-{"version":3,"sources":["../ES2015/business/subCharacteristic.js"],"names":["SubCharacteristic","family","sourceSC","uid","name","id"],"mappings":";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;QAOMA,iB;AAEF,mCAAYC,MAAZ,EAAoBC,QAApB,EAA6B;AAAA;;AACzB,gBAAID,MAAJ,EACI,KAAKA,MAAL,GAAcA,MAAd;AACJ,gBAAIC,QAAJ,EAAc;AACV,qBAAKC,GAAL,GAAWD,SAASC,GAApB;AACA,qBAAKC,IAAL,GAAYF,SAASE,IAArB;AACH;AACJ;;;;oCAEO;AACJ,uBAAO,KAAKC,EAAZ;AACH;;;kCAEKA,E,EAAI;AACN,qBAAKA,EAAL,GAAUA,EAAV;AACH;;;qCAEQ;AACL,uBAAO,KAAKF,GAAZ;AACH;;;mCAEMA,G,EAAK;AACR,qBAAKA,GAAL,GAAWA,GAAX;AACH;;;sCAES;AACN,uBAAO,KAAKC,IAAZ;AACH;;;oCAEOA,I,EAAM;AACV,qBAAKA,IAAL,GAAYA,IAAZ;AACH;;;wCAEW;AACR,uBAAO,KAAKH,MAAZ;AACH;;;sCAESA,M,EAAQ;AACd,qBAAKA,MAAL,GAAcA,MAAd;AACH;;;;;;YAGGD,iB,GAAAA,iB","file":"subCharacteristic.js","sourcesContent":["/**\n * Created by Thierry Hubmann\n *\n * Cette classe est développée en ES2015 et est recompilée avec Babel\n * C'est la classe business de la sous-caractéristique\n */\n\nclass SubCharacteristic {\n\n    constructor(family, sourceSC){\n        if (family)\n            this.family = family;\n        if (sourceSC) {\n            this.uid = sourceSC.uid;\n            this.name = sourceSC.name;\n        }\n    }\n\n    getId() {\n        return this.id;\n    }\n\n    setId(id) {\n        this.id = id;\n    }\n\n    getUid() {\n        return this.uid;\n    }\n\n    setUid(uid) {\n        this.uid = uid;\n    }\n\n    getName() {\n        return this.name;\n    }\n\n    setName(name) {\n        this.name = name;\n    }\n\n    getFamily() {\n        return this.family;\n    }\n\n    setFamily(family) {\n        this.family = family;\n    }\n}\n\nexport {SubCharacteristic};\n"]}
+{"version":3,"sources":["../ES2015/business/subCharacteristic.js"],"names":["SubCharacteristic","family","sourceSC","uid","name","real_name","id"],"mappings":";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;QAOMA,iB;AACF,mCAAYC,MAAZ,EAAoBC,QAApB,EAA6B;AAAA;;AACzB,gBAAID,MAAJ,EACI,KAAKA,MAAL,GAAcA,MAAd;AACJ,gBAAIC,QAAJ,EAAc;AACV,oBAAI,SAASA,QAAb,EACA;AACI,yBAAKC,GAAL,GAAWD,SAASC,GAApB;AACA,yBAAKC,IAAL,GAAYF,SAASE,IAArB;AACH,iBAJD,MAMA;AACI,yBAAKD,GAAL,GAAWD,SAASE,IAApB;AACA,yBAAKA,IAAL,GAAYF,SAASG,SAArB;AACH;AACJ;AACJ;;;;oCAEO;AACJ,uBAAO,KAAKC,EAAZ;AACH;;;kCAEKA,E,EAAI;AACN,qBAAKA,EAAL,GAAUA,EAAV;AACH;;;qCAEQ;AACL,uBAAO,KAAKH,GAAZ;AACH;;;mCAEMA,G,EAAK;AACR,qBAAKA,GAAL,GAAWA,GAAX;AACH;;;sCAES;AACN,uBAAO,KAAKC,IAAZ;AACH;;;oCAEOA,I,EAAM;AACV,qBAAKA,IAAL,GAAYA,IAAZ;AACH;;;wCAEW;AACR,uBAAO,KAAKH,MAAZ;AACH;;;sCAESA,M,EAAQ;AACd,qBAAKA,MAAL,GAAcA,MAAd;AACH;;;;;;YAGGD,iB,GAAAA,iB","file":"subCharacteristic.js","sourcesContent":["/**\n * Created by Thierry Hubmann\n *\n * Cette classe est développée en ES2015 et est recompilée avec Babel\n * C'est la classe business de la sous-caractéristique\n */\n\nclass SubCharacteristic {\n    constructor(family, sourceSC){\n        if (family)\n            this.family = family;\n        if (sourceSC) {\n            if ('uid' in sourceSC)\n            {\n                this.uid = sourceSC.uid ;\n                this.name = sourceSC.name;\n            }\n            else\n            {\n                this.uid = sourceSC.name ;\n                this.name = sourceSC.real_name;\n            }\n        }\n    }\n\n    getId() {\n        return this.id;\n    }\n\n    setId(id) {\n        this.id = id;\n    }\n\n    getUid() {\n        return this.uid;\n    }\n\n    setUid(uid) {\n        this.uid = uid;\n    }\n\n    getName() {\n        return this.name;\n    }\n\n    setName(name) {\n        this.name = name;\n    }\n\n    getFamily() {\n        return this.family;\n    }\n\n    setFamily(family) {\n        this.family = family;\n    }\n}\n\nexport {SubCharacteristic};\n"]}

commit 6f80bfadae867a78041994fb39d072f081672816

Fixes strata duplication on stratigraphy save bug #MC-333 Add Work Development 2h Fixes strata duplication on stratigraphy save bug
-  neo4jdao.py Bug introduced by commit f5bf622 refactoring deleteAllStrataFromAStratigraphy
   due to missing optional match only strata in stratigraphy having parents and children strata would
   be deleted.

micorr/stratigraphies/neo4jdao.py
@@ -344,7 +344,8 @@ class Neo4jDAO:
     def deleteAllStrataFromAStratigraphy(self, stratigraphy):
         # supression des strates enfants, interfaces et components
         self.graph.cypher.execute('''
-            MATCH (sg:Stratigraphy {uid:{stratigraphy}})-[:POSSESSES]->(parent_st:Strata)-[:IS_PARENT_OF]->(child_st:Strata)
+            MATCH (sg:Stratigraphy {uid:{stratigraphy}})
+            OPTIONAL MATCH (sg)-[:POSSESSES]->(parent_st:Strata)-[:IS_PARENT_OF]->(child_st:Strata)
             DETACH DELETE child_st
             WITH sg MATCH (sg)-[:POSSESSES]->(st:Strata)-[:HAS_UPPER_INTERFACE]->(i:Interface)
             OPTIONAL MATCH (st)-[:INCLUDES]->(c:Component)