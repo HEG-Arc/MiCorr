218d6963ab520864f1eb97cf244e1d5a5e36012c
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2018-05-17 23:12:38 +0200

    #MC-334 Add Work Development 15m Refactors stratTexture.js as stratInterface.js in previous commit
     - Refactors stratTexture.js as stratInterface.js in previous commit

--- micorr/stratigraphies/ngapp/src/controllers/stratTexture.js
+++ micorr/stratigraphies/ngapp/src/controllers/stratTexture.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import {Characteristic} from "../business/characteristic";
-import {getCharacteristicByItsName} from "../init";
+import {getSelectedFamilyCharacteristic} from "../init";
 
 /**
  * @ngdoc function
@@ -13,51 +13,26 @@ import {getCharacteristicByItsName} from "../init";
 angular.module('micorrApp')
     .controller('StratTextureCtrl', function ($scope, $route, $window, StratigraphyData) {
 
-
-        var initStratTexture = function () {
-            $scope.porosityFamily = StratigraphyData.getPorosityFamily()['characteristics'];
-            $scope.cohesionFamily = StratigraphyData.getCohesionFamily()['characteristics'];
-            $scope.hardnessFamily = StratigraphyData.getHardnessFamily()['characteristics'];
-            $scope.crackingFamily = StratigraphyData.getCrackingFamily()['characteristics'];
-            $scope.descriptions = StratigraphyData.descriptions;
-        };
-
+        const textureFamilies = ["porosityFamily","cohesionFamily", "hardnessFamily", "crackingFamily"];
         $scope.$on('initShowStrat', function (event) {
-            initStratTexture();
+            for (let f of textureFamilies)
+                $scope[f] = StratigraphyData[f].characteristics;
+            $scope.selected = {};
+            for (let f of textureFamilies)
+                $scope.selected[f] = null;
+            $scope.descriptions = StratigraphyData.descriptions;
         });
 
-        /**
-         * Cette fonction vide les champs et est appelée à chaque chargement du formulaire
-         * pour éviter de garder des anciennes valeurs
-         */
-        function emptyFields() {
-            $scope.selectedPorosityFamily = null;
-            $scope.selectedCohesionFamily = null;
-            $scope.selectedHardnessFamily = null;
-            $scope.selectedCrackingFamily = null;
-        }
 
         /* Met à jour les données de la strate en fonction des valeurs dans le formulaire
          * @params
          * @returns
          */
         $scope.$on('updateTexture', function () {
-            emptyFields();
-
-            var strata = StratigraphyData.getStratigraphy().getStratas()[StratigraphyData.getSelectedStrata()];
-
-            if (strata.getCharacteristicsByFamily("porosityFamily").length > 0) {
-                $scope.selectedPorosityFamily = getCharacteristicByItsName($scope.porosityFamily, strata.getCharacteristicsByFamily("porosityFamily")[0].getName());
-            }
-            if (strata.getCharacteristicsByFamily("cohesionFamily").length > 0) {
-                $scope.selectedCohesionFamily = getCharacteristicByItsName($scope.cohesionFamily, strata.getCharacteristicsByFamily("cohesionFamily")[0].getName());
-            }
-            if (strata.getCharacteristicsByFamily("hardnessFamily").length > 0) {
-                $scope.selectedHardnessFamily = getCharacteristicByItsName($scope.hardnessFamily, strata.getCharacteristicsByFamily("hardnessFamily")[0].getName());
-            }
-            if (strata.getCharacteristicsByFamily("crackingFamily").length > 0) {
-                $scope.selectedCrackingFamily = getCharacteristicByItsName($scope.crackingFamily, strata.getCharacteristicsByFamily("crackingFamily")[0].getName());
-            }
+            let strata = StratigraphyData.getStratigraphy().getStratas()[StratigraphyData.getSelectedStrata()];
+            for (let f of textureFamilies)
+                $scope.selected[f]=getSelectedFamilyCharacteristic(strata, f, $scope[f]);
+                // e.g $scope.selected["porosityFamily"] = getSelectedFamilyCharacteristic(strata, "porosityFamily", $scope.porosityFamily);
         });
 
         /* Met à jour les données de la strate en fonction des valeurs dans le formulaire
@@ -65,45 +40,10 @@ angular.module('micorrApp')
          * @returns
          */
         $scope.upTexture = function () {
-
-            var strata = StratigraphyData.getStratigraphy().getStratas()[StratigraphyData.getSelectedStrata()];
-
-            if($scope.selectedPorosityFamily != null) {
-                if (strata.findDependency('porosityFamily')) {
-                    var char = new Characteristic();
-                    char.setFamily("porosityFamily");
-                    char.setName($scope.selectedPorosityFamily.name);
-                    char.setRealName($scope.selectedPorosityFamily.real_name);
-                    strata.replaceCharacteristic(char);
-                }
-            }
-            if($scope.selectedCohesionFamily != null) {
-                if (strata.findDependency('cohesionFamily')) {
-                    var char = new Characteristic();
-                    char.setFamily("cohesionFamily");
-                    char.setName($scope.selectedCohesionFamily.name);
-                    char.setRealName($scope.selectedCohesionFamily.real_name);
-                    strata.replaceCharacteristic(char);
-                }
-            }
-            if($scope.selectedHardnessFamily != null) {
-                if (strata.findDependency('hardnessFamily')) {
-                    var char = new Characteristic();
-                    char.setFamily("hardnessFamily");
-                    char.setName($scope.selectedHardnessFamily.name);
-                    char.setRealName($scope.selectedHardnessFamily.real_name);
-                    strata.replaceCharacteristic(char);
-                }
-            }
-            if($scope.selectedCrackingFamily != null) {
-                if (strata.findDependency('crackingFamily')) {
-                    var char = new Characteristic();
-                    char.setFamily("crackingFamily");
-                    char.setName($scope.selectedCrackingFamily.name);
-                    char.setRealName($scope.selectedCrackingFamily.real_name);
-                    strata.replaceCharacteristic(char);
-                }
-            }
-           $scope.$emit('updateSelectedStrata');
+            let strata = StratigraphyData.getStratigraphy().getStratas()[StratigraphyData.getSelectedStrata()];
+            for (let f of textureFamilies)
+                strata.replaceCharacteristic(new Characteristic(f, $scope.selected[f]));
+                // e.g strata.replaceCharacteristic(new Characteristic("porosityFamily", $scope.selected["porosityFamily"]));
+            $scope.$emit('updateSelectedStrata');
         };
     });
