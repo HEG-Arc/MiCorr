d2fe3c543003ada2e1a181ebbc3f6b083b224b07
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2019-12-17 17:34:02 +0100

    #MC-372 Add Work Development 2h refactoring landing_new.html inherits base.html
    - landing_new.html inherits base.html
    - removes unused old assets

--- micorr/static/old/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    /* Initialise bxSlider */
-    $('.bxslider').bxSlider({
-        captions: true
-    });
-
-    //Apply img-thumbnail class to body-content images
-    $('.body-content img').addClass("img-thumbnail");
-});
