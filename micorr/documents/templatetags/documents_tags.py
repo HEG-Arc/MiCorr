c19c617ef4ddf87bd506a7943780f5c852a9be37
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2020-04-16 21:29:34 +0200

    #MC-356 Add Work Development 15m replaces assignment_tag helper removed in django 2

--- micorr/documents/templatetags/documents_tags.py
+++ micorr/documents/templatetags/documents_tags.py
@@ -37,12 +37,12 @@ register = template.Library()
 
 
 # settings value
-@register.assignment_tag
+@register.simple_tag
 def get_googe_maps_key():
     return getattr(settings, 'GOOGLE_MAPS_KEY', "")
 
 
-@register.assignment_tag(takes_context=True)
+@register.simple_tag(takes_context=True)
 def get_site_root(context):
     # NB this returns a core.Page, not the implementation-specific model used
     # so object-comparison to self will return false as objects would differ
