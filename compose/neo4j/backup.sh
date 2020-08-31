6eb2fbf83d6462c4a2654b1f1ae70641d6107c9c
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2020-08-31 08:56:56 +0200

    #MC-383 Add Work Development 1h -  updates neo4j and node Dockerfile - CAUTION
     - Updating neo4j to 4.0 data require intermediate upgrade to latest 3.4 from backup first before switching neo4j to 4.0

--- compose/neo4j/backup.sh
+++ compose/neo4j/backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 BACKUP_DATE=$(date +"%Y%m%d_%H%M%S")
 echo "neo4j-admin dump to: /data/graphdb_${BACKUP_DATE}.dump"
-/var/lib/neo4j/bin/neo4j-admin dump --database=graph.db --to=/data/graphdb_${BACKUP_DATE}.dump
+/var/lib/neo4j/bin/neo4j-admin dump --database=neo4j --to=/data/graphdb_${BACKUP_DATE}.dump
 
