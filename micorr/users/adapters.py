0124c9f9f1abc53d78141e7762d99bc02628d3a7
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2021-06-01 18:59:45 +0200

    Adds new registered users to Micorr Editors
    - also adds Data migration to create MiCorr Editors group

--- micorr/users/adapters.py
+++ micorr/users/adapters.py
@@ -2,11 +2,21 @@
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.contrib.auth.models import Group
 
 
 class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
+
+    def save_user(self, request, user, form, commit=True):
+        user = super(AccountAdapter, self).save_user(request, user, form, commit=False)
+
+        # add newly registered users to MiCorr Editors group by default
+        micorr_editors, created = Group.objects.get_or_create(name='MiCorr Editors')
+        user.save()
+        micorr_editors.user_set.add(user)
+        return user
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
