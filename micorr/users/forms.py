4ee9620f1a3e7a11122bf0c5d87713f2e555c9d0
Bernard Letourmy <bernard.letourmy@he-arc.ch>
2021-06-03 16:04:15 +0200

    changes captcha position in signup form

--- micorr/users/forms.py
+++ micorr/users/forms.py
@@ -20,6 +20,14 @@ class CaptchaSignupForm(SignupForm):
     captcha = CaptchaField(widget=CaptchaTextInput(attrs={'class': 'form-control textInput',
                                                           'placeholder': 'Captcha content'}))
 
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        field_order = list(self.fields.keys())
+        # get current fields and move catpcha to the end
+        field_order.remove('captcha')
+        field_order.append('captcha')
+        self.order_fields(field_order)
+
     def save(self, request):
 
         # Ensure you call the parent class's save.
